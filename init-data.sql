6da91ba1e4b7884c776b22711583deea3bb584c1
Stellupo <e.braido@gmail.com>
2021-02-09 17:28:10 +0100

    API Marvel : stage 2 - DB updates

--- init-data.sql
+++ init-data.sql
@@ -32,24 +32,24 @@ INSERT INTO Character (id, name, story, abilities, img, header)
                'WOLVERINE',
                'From the northern wilderness of Canada hails one of the gruffest, most irascible, totally cynical and brooding member of the X-Men ever to grace the team with his presence.',
                'Indestructible claws,Healing powers,Enhanced senses',
-               'assets/images/wolverine.jpg',
-               'assets/images/wolverine_header.png'
+               'assets/images/Characters/wolverine.jpg',
+               'assets/images/Characters/wolverine_header.png'
            ),
            (
                2,
-               'Storm',
-               'White haired cool girl',
-               'Weather control, Flight',
-               'assets/images/storm.jpg',
-               'assets/images/storm_header.png'
-           ),
-           (
-               3,
                'IRON MAN',
                'Having created a wondrous suit of armor to keep himself alive, Tony has revised it dozens of times, each version with increased capabilities, faster operation, reduced energy usage, and adaptability to the greatest threats the universe has to offer.',
                '360 vision,Energy blast,Powerful Armor',
-               'assets/images/Ironman.jpg',
-               'assets/images/ironman_header.png'
+               'assets/images/Characters/Ironman.jpg',
+               'assets/images/Characters/ironman_header.png'
+           ),
+          (
+               3,
+               'GROOT',
+               'This sentient alien tree branches out of his comfort zone to help the Guardians of the Galaxy keep the people of the universe safe.',
+               'Regeneration', 'Super strength',
+                'assets/images/Characters/groot.png',
+                'assets/images/Characters/groot_header.png'
            );
 
 INSERT INTO "Group" (id, name, story, img, header)
@@ -57,29 +57,29 @@ INSERT INTO "Group" (id, name, story, img, header)
                1,
                'X-MEN',
                'Realizing mutants were the next step in human evolution, Professor Charles Xavier gathered gifted teenagers to be his first class of students. His goal to protect and educate the next generation of homo superior, while pursuing a dream of harmony.',
-               'assets/images/x-men.jpg',
-               'assets/images/xmen_header.jpg'
+               'assets/images/Groups/x-men.jpg',
+               'assets/images/Groups/xmen_header.jpg'
            ),
            (
                2,
                'Guardians of the galaxy',
                'This group of intergalactic outlaws, turned unlikely saviors of the galaxy, are typically without a plan and bicker constantly. But they care deeply about one another and usually get the job done, legal or otherwise.',
-               'assets/images/guardian_of_the_galaxy.jpg',
-               'assets/images/guardian_of_the_galaxy_header.jpg'
+               'assets/images/Groups/guardian_of_the_galaxy.jpg',
+               'assets/images/Groups/guardian_of_the_galaxy_header.jpg'
            ),
            (
                3,
                'Avengers',
                'The Avengers began as a group of extraordinary individuals who were assembled to defeat Loki and his Chitauri army in New York City. Since then, the team has expanded its roster and faced a host of new threats, while dealing with their own turmoil.',
-               'assets/images/avengers.jpg',
-               'assets/images/avengers_header.jpg'
+               'assets/images/Groups/avengers_header.jpg',
+               'assets/images/Groups/avengers_header.jpg'
            ),
            (
                4,
-               'SHIELD',
-               'blabla',
-               'assets/images/shield.jpg',
-               'assets/images/shield_header.jpg'
+               'S.H.I.E.L.D',
+               'S.H.I.E.L.D. is an espionage and security organization that defends Earth and its people from groups that pose advanced technological—and sometimes supernatural or extraterrestrial—threats',
+               'assets/images/Groups/shield.jpg',
+               'assets/images/Groups/shield_header.jpg'
            );
 
 INSERT INTO GroupMember (id, character_id, group_id)
@@ -91,15 +91,15 @@ INSERT INTO GroupMember (id, character_id, group_id)
            (
                2,
                2,
-               1
+               3
            ),
            (
                3,
-               3,
-               3
+               2,
+               4
            ),
            (
                4,
                3,
-               4
+               2
            );
